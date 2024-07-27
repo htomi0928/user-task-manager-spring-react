f1b269a20c42ecf37558e1ad5255c05ae5255fa4
Hollósi Tamás <tomi.hollosi@gmail.com>
2024-07-27 23:56:20 +0200

    fixed update, fixed frontend Cors permit things

--- src/main/java/com/bdo/digital/user_task_service/service/UserService.java
+++ src/main/java/com/bdo/digital/user_task_service/service/UserService.java
@@ -8,6 +8,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -33,13 +35,36 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public User updateUser(long id, User user) {
-        if (userRepository.existsById(id)) {
-            user.setId(id);
+    public User updateUser(long id, User updatedUser) {
+        User user = userRepository.findById(id).orElse(null);
+        if(user != null){
+            user.setName(updatedUser.getName());
+            user.setEmail(updatedUser.getEmail());
+            user.setPassword(updatedUser.getPassword());
+            user.setAddress(updatedUser.getAddress());
             user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+            // Merge tasks
+            if (updatedUser.getTasks() != null) {
+                Map<Long, Task> existingTasksMap = user.getTasks().stream()
+                        .collect(Collectors.toMap(Task::getId, task -> task));
+
+                for (Task task : updatedUser.getTasks()) {
+                    if (existingTasksMap.containsKey(task.getId())) {
+                        Task existingTask = existingTasksMap.get(task.getId());
+                        existingTask.setTitle(task.getTitle());
+                        existingTask.setDescription(task.getDescription());
+                        existingTask.setDeleted(task.isDeleted());
+                    } else {
+                        user.getTasks().add(task);
+                    }
+                }
+            }
             return userRepository.save(user);
         }
-        return null;
+        else{
+            return null;
+        }
     }
 
     public boolean softDeleteUser(long id){
